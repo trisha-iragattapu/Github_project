58622749ea72f67420117d0538ddb885a2dc87dd
trisha-iragattapu <iragattaputrisha@gmail.com>
2026-04-24 12:12:23 +0530

    Added new line

--- src/com/java/FirstProgram.java
+++ src/com/java/FirstProgram.java
@@ -1,5 +1,9 @@
 package com.java;
 
 public class FirstProgram {
-
+	
+      void print() {
+    	  System.out.println("welcome back to github session2");
+    	  System.out.println("hello guys, u want biriyani");
+      }
 }
